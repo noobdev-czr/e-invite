6bdb3f4544a70dc78aded445ba9a54dbcb7563d5
Cezar Ryan Umbana <cezar@intelpeek.com>
2026-01-14 17:55:48 +0400

    Final Changes

--- wedding-invite/src/components/GalleryCarousel.tsx
+++ wedding-invite/src/components/GalleryCarousel.tsx
@@ -1,4 +1,6 @@
 import useEmblaCarousel from "embla-carousel-react";
+import { gallery1, gallery2, gallery3, gallery4, gallery5,gallery6 } from "../assets/images";
+import ImageBox from "./ImageBox";
 
 export default function GalleryCarousel() {
   const [emblaRef] = useEmblaCarousel({ loop: true });
@@ -6,11 +8,13 @@ export default function GalleryCarousel() {
   return (
     <div ref={emblaRef} className="overflow-hidden">
       <div className="flex gap-4">
-        {[1, 2, 3, 4].map((i) => (
+        {[gallery1, gallery2, gallery3,gallery4,gallery5,gallery6].map((img, i) => (
           <div
             key={i}
-            className="flex-[0_0_80%] h-48 bg-sage rounded-xl"
-          />
+            className="flex-[0_0_90%] sm:flex-[0_0_45%] lg:flex-[0_0_30%]"
+          >
+            <ImageBox src={img} />
+          </div>
         ))}
       </div>
     </div>
