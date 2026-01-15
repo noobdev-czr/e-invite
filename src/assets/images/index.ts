11b526b4adeee3b4f8e6c68286160ed57a50c6e9
Cezar Ryan Umbana <cezar@intelpeek.com>
2026-01-15 15:48:01 +0400

    Add Gift Details

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -9,6 +9,10 @@ import gallery5 from "./gallery5.jpg";
 import gallery6 from "./gallery6.jpg";
 import gifts from "./gifts.jpg";
 import footer from "./footer.jpg";
+import Qr2 from "./Qr2.jpeg";
+import Qr1 from "./Qr1.jpeg";
+// import Qr1 from "./Qr1.jpeg";
+// import Qr2 from "./Qr1.jpg";
 
 export {
   hero,
@@ -22,4 +26,6 @@ export {
   gallery6,
   gifts,
   footer,
+  Qr1,
+  Qr2,
 };
